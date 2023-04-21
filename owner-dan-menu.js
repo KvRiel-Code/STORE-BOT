0cc901e328a137461665a7eb2368d6065609f5d6
KvRiel-Code2 <lelyrioeh99@gmail.com>
2023-04-21 21:43:58 +0800

    v5+module

---
+++ owner-dan-menu.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+
+global.namabot = "Assistant KvRiel"
+global.namaowner = "KvRiel-Code"
+global.footer_text = "© " + namabot
+global.pp_bot = fs.readFileSync("./image/foto.jpg")
+global.qris = fs.readFileSync("./image/qris.jpg")
+global.owner = ['6281344956704','6281242209942']
+global.sessionName = 'session'
+global.prefa = ['-_-']
+global.caption_pay = `Qris All Pay
+_QRIS ini khusus untuk Donasi saja`
+//menu bot rapihin sendiri ya, belajar lah jadi anak mandiri.
+module.exports.helpMenu = (pushname) =>{
+  return `Halo ${pushname}
+
+*「 FITUR BOT 」*
+
+\`\`\`» /owner
+» /addsewa
+» /delsewa
+» /ceksewa
+» /listsewa
+» /pay
+» /setpay
+» /list
+» /addlist
+» /updatelist
+» /renamelist
+» /dellist
+» /jeda
+» /tambah
+» /kurang
+» /kali
+» /bagi
+» /setproses
+» /changeproses
+» /delsetproses
+» /setdone
+» /changedone
+» /delsetdone
+» /proses
+» /done
+» /welcome
+» /goodbye
+» /setwelcome
+» /changewelcome
+» /delsetwelcome
+» /setleft
+» /changeleft
+» /delsetleft
+» /antiwame
+» /antiwame2
+» /antilink
+» /antilink2
+» /open
+» /close
+» /hidetag
+» /add
+» /kick
+» /stiker
+» /ffid
+» /mlid
+» /setppgc
+» /setnamegc
+» /setdesgc
+» /linkgc
+» /resetlinkgc
+» /promote
+» /demote
+» /setbot
+» /updatesetbot
+» /delsetbot
+» /bot
+\`\`\`\`
+}
